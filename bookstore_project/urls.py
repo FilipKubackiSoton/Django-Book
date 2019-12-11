2b376539236043b05de2e1091ac01fb15d90bd88
FilipKubackiSoton <fk1g19@soton.ac.uk>
2019-12-11 11:59:22 +0100

    'ch5'

--- bookstore_project/urls.py
+++ bookstore_project/urls.py
@@ -17,6 +17,16 @@ from django.contrib import admin
 from django.urls import path, include #new
 
 urlpatterns = [
+    #Django admin
     path('admin/', admin.site.urls),
+
+    #user management
+    path('accounts/', include('django.contrib.auth.urls')), #new
+
+    #local apps
+    path('accounts/', include('users.urls')), #new
     path('', include('pages.urls')), #new
+
+
+
 ]
