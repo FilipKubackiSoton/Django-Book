ffeb18968a2ff2f0b257fb8fc1c0226aca7086d5
FilipKubackiSoton <fk1g19@soton.ac.uk>
2019-12-12 10:53:34 +0100

    'ch6'

--- bookstore_project/settings.py
+++ bookstore_project/settings.py
@@ -38,10 +38,16 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+    #third-party
+    'crispy_forms', #new
+
+
     #local
     'users.apps.UsersConfig',
     'pages.apps.PagesConfig', #new
 ]
+#django-crispy-forms
+CRISPY_TEMPLATE_PACK = 'bootstrap4' # new
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -125,7 +131,15 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
+#static settings
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'),]#new
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_FINDERS = [
+    "django.contrib.staticfiles.finders.FileSystemFinder",
+    "django.contrib.staticfiles.finders.AppDirectoriesFinder",
+]
+
 
 AUTH_USER_MODEL = 'users.CustomUser' #new
 
