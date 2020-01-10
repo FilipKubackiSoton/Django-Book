d2834d48f0fcf9d4ad925405f0d521e7eb1be8f7
FilipKubackiSoton <fk1g19@soton.ac.uk>
2020-01-10 20:27:15 +0100

    'ch13'

--- books/views.py
+++ books/views.py
@@ -1,13 +1,17 @@
 from django.shortcuts import render
+from django.contrib.auth.mixins import (LoginRequiredMixin, PermissionRequiredMixin) #new
 from django.views.generic import ListView, DetailView
 from .models import Book
 
-class BookListView(ListView):
+class BookListView(LoginRequiredMixin, ListView):
     model = Book
-    contex_object_name = 'book_list'#new
+    contex_object_name = 'book_list'
     template_name = 'books/book_list.html'
+    login_url ='account_login' #new
 
-class BookDetailView(DetailView):
+class BookDetailView(LoginRequiredMixin, PermissionRequiredMixin, DetailView):
     model = Book
     contex_object_name = 'book'
     template_name = 'books/book_detail.html'
+    login_url = 'account_login' #new
+    permission_required = 'books.special_status'
